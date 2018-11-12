eb01fbe8bc1153d67be4f2ded1ad1885b963109a
Aramayis Orkusyan <aramayis@localhost.localdomain>
2018-11-12 13:54:47 -0800

    chore: Add comments documenting BBQ interface.

--- BBQ.h
+++ BBQ.h
@@ -4,7 +4,7 @@
 #include <mutex>
 #include <condition_variable>
 
-static const int MAX = 10;
+static const int MAX = 20;
 
 class BBQ
 {
@@ -24,9 +24,32 @@ private:
     inline bool canRemove() { return m_head < m_tail; }
     
 public:
+    /**
+     * Default constructor
+     */
     BBQ();
+
+    /**
+     * Default destructor.
+     */
     ~BBQ() {};
+
+    /**
+     * Inserts a new item into the buffer if buffer is not full. Otherwise,
+     * waits until space becomes available.
+     * 
+     * @param item - Item to be inserted.
+     * @return Returns the position at which the item was inserted.
+     */
     int insert(int item);
+
+    /**
+     * Removes an item from the buffer if buffer is not empty. Otherwise,
+     * waits until there is an available item in the buffer.
+     * 
+     * @param item - Output parameter to which removed item is copied.
+     * @return Returns position of removed item.
+     */
     int remove(int &item);
 };
 
