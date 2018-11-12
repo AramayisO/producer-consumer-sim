b3a172f7a5248046d3b0e1ff1ee00d67c5f406de
Aramayis Orkusyan <aramayis@localhost.localdomain>
2018-11-11 19:22:38 -0800

    feat: Add BBQ class definition.

---
+++ BBQ.h
@@ -0,0 +1,29 @@
+#ifndef BBQ_H
+#define BBQ_H
+
+#include <mutex>
+#include <condition_variable>
+
+const int MAX = 10;
+
+class BBQ
+{
+private:
+    // Synchronization variables
+    std::mutex m_mutex;
+    std::condition_variable m_itemAdded;
+    std::condition_variable m_itemRemoved;
+
+    // State variables
+    int m_items[MAX];
+    int m_head;
+    int m_tail;
+
+public:
+    BBQ();
+    ~BBQ() {};
+    void insert(int item);
+    int remove();
+};
+
+#endif
