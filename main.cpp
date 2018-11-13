0f3e809a0c10eb755ede1402a97209b36d53a092
Aramayis Orkusyan <coffeemeetscode@gmail.com>
2018-11-13 10:37:54 -0800

    feat: Modify threads to change random sleep time at each iteration.
    Add documentaiton for main driver code.

--- main.cpp
+++ main.cpp
@@ -9,31 +9,61 @@
 constexpr int NUM_PRODUCERS{10};
 constexpr int NUM_CONSUMERS{10};
 
-void producerTask(const BBQ &arg, int max_sleep_time_ms)
+/**
+ * Infinitely looping task that inserts a random value at each iteration into a
+ * shared bounded buffer queue. The thread will sleep for some random time in the
+ * range (0, max_sleep_time_ms) milliseconds between consecutive insert operations.
+ * 
+ * @param _bbq Bounded buffer queue in which values will be inserted.
+ * 
+ * @param max_sleep_time_ms Upper bound for the amound of time the thread will
+ *                          between consecutive insert operations in milliseconds.
+ */
+void producerTask(const BBQ &_bbq, int max_sleep_time_ms)
 {
-    BBQ &bbq = const_cast<BBQ&>(arg);
-    int sleep_time_ms = std::rand() % max_sleep_time_ms;
+    BBQ &bbq = const_cast<BBQ&>(_bbq);
+    
     while (true)
     {
         int item_id = bbq.insert(std::rand());
         std::printf("Item ID %d produced by thread number 0x%x\n", item_id, std::this_thread::get_id());
-        std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time_ms));
+        std::this_thread::sleep_for(std::chrono::milliseconds(std::rand() % max_sleep_time_ms));
     }
 }
 
+/**
+ * Infinitely looping task that removes a value at each iteration from a shared
+ * bounded buffer queue. The thread will sleep for some random time in the range
+ * (0, max_sleep_time_ms) milliseconds between consecutive remove operations
+ * 
+ * @param _bbq Bounded buffer queue from which values will be removed.
+ * 
+ * @param max_sleep_time_ms Upper bound for the amound of time the thread will
+ *                          between consecutive remove operations in milliseconds.
+ */
 void consumerTask(const BBQ &arg, int max_sleep_time_ms)
 {
     BBQ &bbq = const_cast<BBQ&>(arg);
-    int sleep_time_ms = std::rand() % max_sleep_time_ms;
+    
     while (true)
     {
         int item;
         int item_id = bbq.remove(item);
         std::printf("Item ID %d consumed by thread number 0x%x\n", item_id, std::this_thread::get_id());
-        std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time_ms));
+        std::this_thread::sleep_for(std::chrono::milliseconds(std::rand() % max_sleep_time_ms));
     }
 }
 
+// ============================================================================
+// = Main: Entry point into the program.
+// =       Creates NUM_PRODUCER producing threads and NUM_CONSUMER consuming
+// =       that share an instance of a BBQ. Each thread runs indefinitely with 
+// =       a random sleep time between consecutive insert and remove operations
+// =       within the range (0, producers_max_sleep_time_ms) and 
+// =       (0, consumer_max_sleep_time_ms) where producers_max_sleep_time_ms 
+// =       and consumer_max_sleep_time_ms are in milliseconds and passed in as
+// =       command-line arguments.
+// ============================================================================
 int main(int argc, char **argv)
 {
     // Check if program was called with correct number of command-line arguemnts.
@@ -62,16 +92,15 @@ int main(int argc, char **argv)
     // Create producing threads.
     for (int i = 0; i < NUM_PRODUCERS; ++i) {
         producers.push_back(std::thread(producerTask, std::ref(bbq), producers_max_sleep_time_ms));
-        std::printf("Created producer thread %d\n", i);
+        // std::printf("Created producer thread %d\n", i);
 
     }
 
     // Create consuming threads.
     for (int i = 0; i < NUM_CONSUMERS; ++i) {
         consumers.push_back(std::thread(consumerTask, std::ref(bbq), consumers_max_sleep_time_ms));
-        std::printf("Created consumer thread %d\n", i);
+        // std::printf("Created consumer thread %d\n", i);
     }
-
 
     // Even though the threads will run forever, we need to call join on the 
     // threads so that the main thread will wait and not exit till the
